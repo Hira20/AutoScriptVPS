3e6bef6e5ae50339c57c0c762861ab1f4520ed65
jvb20 <48585021+jvb20@users.noreply.github.com>
2019-03-15 21:36:32 +0800

    Update create.sh

--- Files/Menu/create.sh
+++ Files/Menu/create.sh
@@ -59,7 +59,7 @@ echo -e "         Port OpenSSH    :  "$opensshport
 echo -e "         Port Dropbear   :  "$dropbearport
 echo -e "         Port SSL        :  "$stunnel4port
 echo -e "         Port Squid      :  "$squidport
-echo -e "         Port OpenVPN    :  1194
+echo -e "         Port OpenVPN    :  "1194
 echo -e "              http://$MYIP/client.ovpn"
 echo -e "                                                  "
 echo -e "\e[94m[][][]======================================[][][]\e[0m"
